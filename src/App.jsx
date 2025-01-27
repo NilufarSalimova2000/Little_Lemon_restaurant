a305142ed7f7f3fba71cebb9f2395508f708f8b2
NilufarSalimova2000 <nilufarsalimova2000@gmail.com>
2025-01-28 00:01:28 +0500

    project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { BookingForm } from './components/booking-form';
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Little Lemon Restaurant</h1>
+      <BookingForm />
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/booking-form/booking-form.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+
+export function BookingForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [guests, setGuests] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !guests || !date || !time) {
+      setError('Please fill in all fields.');
+      return;
+    }
+
+    // Rezervatsiya muvaffaqiyatli
+    setError('');
+    alert('Booking successful!');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Table Booking Form</h2>
+
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="guests">Number of Guests:</label>
+        <input
+          type="number"
+          id="guests"
+          value={guests}
+          onChange={(e) => setGuests(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="date">Date:</label>
+        <input
+          type="date"
+          id="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="time">Time:</label>
+        <input
+          type="time"
+          id="time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+          required
+        />
+      </div>
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+
+      <button type="submit">Book Now</button>
+    </form>
+  );
+}
